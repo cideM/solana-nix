a3b7430ead7e2b3eb6a13038aae70487d191869f
Florian Beeres <yuuki@protonmail.com>
2021-10-10 21:39:23 +0200

    work in progress

--- flake.nix
+++ flake.nix
@@ -11,14 +11,25 @@
       let
         pkgs = import nixpkgs { inherit system; };
 
+        # https://github.com/solana-labs/solana/blob/master/scripts/cargo-install-all.sh#L71
+        endUserBins = [
+          "cargo-build-bpf"
+          "cargo-test-bpf"
+          "solana"
+          "solana-install"
+          "solana-install-init"
+          "solana-keygen"
+          "solana-stake-accounts"
+          "solana-test-validator"
+          "solana-tokens"
+        ];
+
         meta = with pkgs.stdenv; with pkgs.lib; {
           homepage = "https://solana.com/";
           description = "Solana is a decentralized blockchain built to enable scalable, user-friendly apps for the world.";
           platforms = platforms.unix ++ platforms.darwin;
         };
 
-        # I don't know how to build from source on Darwin
-        # https://discourse.nixos.org/t/ld-framework-not-found-system/15096
         # On NixOS the build got to the test stage but then failed at:
         #
         # last 10 log lines:
@@ -37,93 +48,48 @@
         # Here's an unfinished attempt at adding solana to Nixpkgs where the
         # person had to remove some tests and comment some out.
         # https://github.com/NixOS/nixpkgs/pull/121009/files
-        solanaFromSource = pkgs.rustPlatform.buildRustPackage rec {
-          inherit meta;
-          pname = "solana";
-          version = "1.8.0";
-          src = solanaSrc;
-          cargoSha256 = "034hdyziagvg63q2lr226qw3myy5458rb82277vkxp1xsbamh70l";
-
-          doCheck = false;
-
-          # I commented the dependencies out one by one and checked if the
-          # build fails. The below is the result of that testing.
-          nativeBuildInputs = [
-            pkgs.rustfmt
-            pkgs.llvmPackages.clang # rocksdb
-            pkgs.protobuf
-            pkgs.llvmPackages.libclang # rocksdb
-            pkgs.pkg-config
-          ];
-
-          buildInputs = [
-            pkgs.hidapi
-            pkgs.llvm
-            pkgs.llvmPackages.libclang
-            pkgs.rustfmt
-            pkgs.openssl
-            pkgs.udev
-            pkgs.zlib
-          ];
-
-          preBuild = ''
-            export LLVM_CONFIG_PATH="${pkgs.llvm}/bin/llvm-config";
-            export LIBCLANG_PATH="${pkgs.llvmPackages.libclang.lib}/lib";
-          '';
-        };
-
-        solanaBin =
-          let sources = {
-            "x86_64-darwin" = pkgs.fetchurl {
-              url = "https://github.com/solana-labs/solana/releases/download/v1.6.27/solana-release-x86_64-apple-darwin.tar.bz2";
-              sha256 = "1n0k0y5ix4y9lxbzllzgi8ax992sksxfbgi8x7inqbpavqhj1jpg";
-            };
-            "aarch64-darwin" = pkgs.fetchurl {
-              url = "https://github.com/solana-labs/solana/releases/download/v1.6.27/solana-release-x86_64-apple-darwin.tar.bz2";
-              sha256 = "1n0k0y5ix4y9lxbzllzgi8ax992sksxfbgi8x7inqbpavqhj1jpg";
-            };
-            "x86_64-linux" = pkgs.fetchurl {
-              url = "https://github.com/solana-labs/solana/releases/download/v1.6.27/solana-release-x86_64-unknown-linux-gnu.tar.bz2";
-              sha256 = "sha256-iBGrDLVCAQJ9NSwMHz2PiSqeMXghPe+q7Zv3BSTlCro=";
-            };
-          };
-          in
-          with pkgs.stdenv; with pkgs.lib; pkgs.stdenv.mkDerivation {
+        solanaFromSource = pkgs.rustPlatform.buildRustPackage
+          rec {
             inherit meta;
+            pname = "solana";
+            version = "1.8.0";
+            src = solanaSrc;
+            cargoSha256 = "0yahdh6pzi6xsikz7z6k72hpqv9fkph2ns3m4v99hm5j61q86nfk";
 
-            name = "solana-cli-bin";
-            version = "1.6.27";
-            src = sources.${system};
+            doCheck = false;
 
-            installPhase = ''
-              runHook postInstall
-
-              mkdir $out
-
-              cp -R ./* $out/
-
-              runHook postInstall
-            '';
-
-            nativeBuildInputs = optional (! isDarwin) pkgs.autoPatchelfHook;
-            autoPatchelfIgnoreMissingDeps = true;
-            # These are missing but I don't think they're in Nixpkgs
-            # > autoPatchelfHook could not satisfy dependency libsgx_uae_service.so wanted by /nix/store/wf6qjpsd7d3fw0cqnxisy82a31lfv404-solana-cli-bin/bin/perf-libs/libsigning.so
-            # > autoPatchelfHook could not satisfy dependency libsgx_urts.so wanted by /nix/store/wf6qjpsd7d3fw0cqnxisy82a31lfv404-solana-cli-bin/bin/perf-libs/libsigning.so
-            buildInputs = with pkgs; optionals (! isDarwin) [
-              openssl
-              stdenv.cc.cc.lib
-              opencl-icd
-              udev
-              pkg-config
+            # I commented the dependencies out one by one and checked if the
+            # build fails. The below is the result of that testing.
+            nativeBuildInputs = [
+              pkgs.rustfmt
+              pkgs.llvm
+              pkgs.clang
+              pkgs.protobuf
+              pkgs.pkg-config
             ];
 
+            buildInputs = [
+              pkgs.hidapi
+              pkgs.llvmPackages.libclang
+              pkgs.rustfmt
+              pkgs.openssl
+              pkgs.zlib
+            ] ++ (pkgs.lib.optionals pkgs.stdenv.isDarwin [
+              pkgs.darwin.apple_sdk.frameworks.System
+            ]) ++ (pkgs.lib.optionals pkgs.stdenv.isLinux pkgs.udev);
+
+            # This is how you should do it for Rust bindgen, instead of the
+            # NIX_LDFLAGS
+            BINDGEN_EXTRA_CLANG_ARGS = "-isystem";
+            LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+            LLVM_CONFIG_PATH = "${pkgs.llvm}/bin/llvm-config";
+
+            cargoBuildFlags = builtins.map (binName: "--bin=${binName}") endUserBins;
           };
 
       in
       rec {
         packages = flake-utils.lib.flattenTree {
-          solana-bin = solanaBin;
           solana = solanaFromSource;
         };
         defaultPackage = packages.solana;
